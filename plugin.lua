586899757bfc08a6ba6480d372e47c7d2e615f4e
Viktor Elofsson <viktor@viktorelofsson.se>
2023-08-16 20:33:34 +0200

    Update folder-monitor to support changed API

--- plugin.lua
+++ plugin.lua
@@ -7,8 +7,30 @@ local config   = require "config"
 local cron     = require "cron"
 local fs       = require "fs"
 local log      = require "log"
+local presets  = require "presets"
+local sessions = require "sessions"
 local sqlite3  = require "sqlite3"
 local torrents = require "torrents"
+
+local function apply(preset, params)
+    if preset == nil then
+        return
+    end
+
+    if preset.download_limit  ~= nil then params.download_limit  = preset.download_limit  end
+    if preset.max_connections ~= nil then params.max_connections = preset.max_connections end
+    if preset.max_uploads     ~= nil then params.max_uploads     = preset.download_limit  end
+    if preset.save_path       ~= nil then params.save_path       = preset.save_path       end
+    if preset.upload_limit    ~= nil then params.upload_limit    = preset.upload_limit    end
+
+    if preset.category ~= nil then
+        params.userdata.category = preset.category
+    end
+
+    if preset.tags ~= nil then
+        params.userdata.tags = preset.tags
+    end
+end
 
 -- small function to check if an array contains
 -- a specific value
@@ -57,27 +79,62 @@ local function check(db)
                 goto next_entry
             end
 
-            local params = {
-                preset    = directory.preset,
-                save_path = directory.save_path
-            }
+            local params = nil
 
-            -- no support for magnet links just yet
             if string.sub(file_buffer, 0, 7) == "magnet:" then
-                log.info("File contents parsed as magnet URI")
-                params.magnet_uri = file_buffer
-            else
-                local torrent = torrents.parse(file_buffer, 'buffer')
+                local trimmed_buffer = file_buffer:gsub("^%s*(.-)%s*$", "%1")
+                local p, err = AddTorrentParams.from_magnet(trimmed_buffer)
 
-                if torrents.has(torrent) then
-                    history:insert(file)
+                if err ~= nil then
+                    log.error("Failed to parse magnet link: "..err)
                     goto next_entry
                 end
 
-                params.ti = torrent
+                params = p
+            else
+                params = AddTorrentParams.new()
+
+                local ti, err = TorrentInfo.from_buffer(file_buffer)
+
+                if err ~= nil then
+                    log.error("Failed to parse torrent file: "..err)
+                    goto next_entry
+                end
+
+                params.ti = ti
             end
 
-            torrents.add(params)
+            local info_hash = nil
+
+            if params.ti ~= nil           then info_hash = params.ti:info_hash() end
+            if params.info_hash.v1 ~= nil then info_hash = params.info_hash      end
+
+            log.debug("Adding torrent with info hash "..info_hash.v1)
+
+            local preset_default = presets.get("default")
+            apply(preset_default, params)
+
+            local preset_dir = nil
+
+            if directory.preset ~= nil then
+                preset_dir = presets.get(directory.preset)
+                apply(preset_dir, params)
+            end
+
+            -- override save path from directory config
+            if directory.save_path ~= nil then params.save_path = directory.save_path end
+
+            local session = nil
+
+            if preset_dir ~= nil and preset_dir.session ~= nil then
+                session = sessions.get(preset_dir.session)
+            elseif preset_default.session ~= nil then
+                session = sessions.get(preset_default.session)
+            else
+                session = sessions.get("default")
+            end
+
+            session:add_torrent(params)
 
             history:insert(file)
 
